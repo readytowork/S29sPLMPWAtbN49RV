ee43752a11591c232d916712dd698d7daf9d6a1e
mjyang <yapdinnerisserved@gmail.com>
2014-04-28 11:13:21 +0800

    lunch time commit workshop/

--- workshop/drawExp02.m
+++ workshop/drawExp02.m
@@ -1,18 +1,18 @@
 % collect and plot for hw2
 close all;
-FILE = {'./exp02result'};
+FILE = {'./exp04_present_exp01v2'};
 fg = 1;
 
 figure(fg); fg = fg + 1;
 dat = dlmread(FILE{1});
-for j = 2:size(dat, 2)
-    ln(j-1) = line(dat(:,1), dat(:,j),'Marker','o','LineWidth',2,'MarkerFaceColor','none','MarkerSize',8,'Color',[j/size(dat, 2) 0 0]);
+for j = 2:size(dat, 1)
+    ln(j-1) = line(dat(1,:), dat(j,:),'Marker','o','LineWidth',2,'MarkerFaceColor','none','MarkerSize',8,'Color',[j/size(dat, 1) 0 0]);
 end
 set(gca, 'yScale', 'log', 'yMinorTick','on');
-L = legend('Far end user', 'Near end user');
-set(L,'FontSize',14);
-xlabel('Distance (m)','FontSize',14,'Color','k');
-ylabel('Psr (%)','FontSize',14,'Color','k');
+L = legend('Near-end user', 'Far-end user');
+set(L,'FontSize',14, 'Location', 'SouthEast');
+xlabel('Power allocation factor (\alpha)','FontSize',14,'Color','k');
+ylabel('Bit error rate','FontSize',14,'Color','k');
 set(gca,'fontsize',14);
 grid on;
 box on;
